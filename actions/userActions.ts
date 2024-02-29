b46a097e846d492a85bd13436c37f4003654b685
adriendelagneau <delagneauadrien@yahoo.fr>
2024-02-29 20:23:48 +0100

    layouts start

--- actions/userActions.ts
+++ actions/userActions.ts
@@ -1,9 +1,11 @@
 'use server'
 
+import { dbConnect } from "@/lib/dbConnect"
 import User from "@/lib/models/User"
 
 export const getUserById = async (id: string) => {
     try {
+        await dbConnect()
         const user = await User.findById(id)
         return JSON.parse(JSON.stringify(user)) ;
     } catch (err) {
