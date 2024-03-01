6fa3eb469551a24151e6b7e293f86a769759c0b2
adriendelagneau <delagneauadrien@yahoo.fr>
2024-03-01 10:16:44 +0100

    counter

--- app/(others)/article/[...slug]/page.tsx
+++ app/(others)/article/[...slug]/page.tsx
@@ -1,5 +1,4 @@
 import { getArticleBySlug } from '@/actions/articlesActions'
-import IncreaseNumberOfViews from '@/components/IncreaseNumberOfViews'
 
 import React from 'react'
 
@@ -17,7 +16,7 @@ console.log(article)
   return (
     <div className='w-full   min-h-[200vh] mx-auto mt-24'>
         <h1 className='text-4xl leading-snug font-large  xl:leading-[1.2]  xl:text-5xl  font-newsreader lg:my-0 my-12 line-clamp-2'>{ article.title}</h1>
-        {article._id && <IncreaseNumberOfViews id={article._id} />}
+       
     </div>
   )
 }
