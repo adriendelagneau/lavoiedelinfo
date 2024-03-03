2082e639e0a641427a8ff30edeb4e69aa9935fb2
adriendelagneau <delagneauadrien@yahoo.fr>
2024-03-03 15:57:15 +0100

    visito

--- app/api/check_visitor/route.ts
+++ app/api/check_visitor/route.ts
@@ -1,19 +1,23 @@
 import Visitor from "@/lib/models/Visitor";
 
 export async function POST(req: Request) {
-  
-    const data = await req.json()
+    try {
+        const data = await req.json()
     
-    console.log(data.ip, "dataip")
-    
-    const visitor = await Visitor.findOne({ ip: data.ip })
-    console.log(visitor, "ww")
-    if (!visitor) {
-        const newVisitor = await Visitor.create({
-            ip: data.ip,
-            countOfViews: 1
-        })
-    }
+        console.log(data.ip, "dataip")
+        
+        const visitor = await Visitor.findOne({ ip: data.ip })
+        console.log(visitor, "ww")
+        if (!visitor) {
+            const newVisitor = await Visitor.create({
+                ip: data.ip,
+                countOfViews: 1
+            })
+        }
+    } catch (err) {
+        console.log(err)
+  }
+ 
 
    
     return new Response("This is a new API route");
