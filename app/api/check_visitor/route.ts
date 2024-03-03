111f469ace8ab264c491d3b66037c88e756b1dd1
adriendelagneau <delagneauadrien@yahoo.fr>
2024-03-03 15:29:45 +0100

    visito

--- app/api/check_visitor/route.ts
+++ app/api/check_visitor/route.ts
@@ -1,6 +1,5 @@
 import Visitor from "@/lib/models/Visitor";
 
-
 export async function POST(req: Request) {
     try {
       const { ip } = await req.json();
@@ -9,23 +8,22 @@ export async function POST(req: Request) {
       const existingVisitor = await Visitor.findOne({ ip });
   
       if (existingVisitor) {
-        // Visitor already exists
-        if (existingVisitor.countOfViews < 3) {
-          // Increment countOfViews if less than 3
-          existingVisitor.countOfViews += 1;
-          await existingVisitor.save();
-          console.log('Incremented numberOfViews for existing visitor:', existingVisitor);
-        } else {
-          // Perform a different action if numberOfViews is 3 or more
-          console.log('numberOfViews is already 3 or more for existing visitor:', existingVisitor);
-          return new Response("Already 3 views", { status: 200 });
-        }
+        if (existingVisitor.numberOfViews < 3) {
+            // Increment numberOfViews if less than 3
+            existingVisitor.numberOfViews += 1;
+            await existingVisitor.save();
+            console.log('Incremented numberOfViews for existing visitor:', existingVisitor);
+          } else {
+            // Perform a different action if numberOfViews is 3 or more
+            console.log('numberOfViews is already 3 or more for existing visitor:', existingVisitor);
+            // Add your logic here
+          }
+  
       } else {
         // Visitor does not exist, create a new one
         const newVisitor = await Visitor.create({
           ip,
           countOfViews: 1,
-          // Add other properties as needed
         });
   
         console.log('New Visitor:', newVisitor);
