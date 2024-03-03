7e621e024816f88964020faaf13b3d98058065e1
adriendelagneau <delagneauadrien@yahoo.fr>
2024-03-03 11:55:45 +0100

    visito

--- app/api/check_visitor/route.ts
+++ app/api/check_visitor/route.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export async function POST(req: Request) {
   
     const data = await req.json()
-console.log(data.ip)
+console.log(data.ip, "dataip")
     const visitor = await Visitor.findOne({ ip: data.ip })
     console.log(visitor, "ww")
     if (!visitor) {
