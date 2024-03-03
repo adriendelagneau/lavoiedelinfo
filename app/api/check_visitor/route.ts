f209d5933d058af1d57fd96e905cae1353756812
adriendelagneau <delagneauadrien@yahoo.fr>
2024-03-03 16:02:23 +0100

    visito

--- app/api/check_visitor/route.ts
+++ app/api/check_visitor/route.ts
@@ -8,12 +8,7 @@ export async function POST(req: Request) {
         
         const visitor = await Visitor.findOne({ ip: data.ip })
         console.log(visitor, "ww")
-        if (!visitor) {
-            const newVisitor = await Visitor.create({
-                ip: data.ip,
-                countOfViews: 1
-            })
-        }
+        
     } catch (err) {
         console.log(err)
   }
